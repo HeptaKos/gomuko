2e70eb3a6000adfd58ad0a337ea5940b958b53a8
齐一先 <2496502521@qq.com>
2022-07-25 12:26:09 +0800

    for git test
    Signed-off-by: qiyixian <2496502521@qq.com>

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 print('git test')
 print('wenhe testing')
-<<<<<<<<< Temporary merge branch 1
 print('git test2')
-=========
 print('git test2')
 print('qiyixian')
 print('qiyixian123')
->>>>>>>>> Temporary merge branch 2
